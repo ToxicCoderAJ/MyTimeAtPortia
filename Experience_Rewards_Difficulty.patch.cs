73b02506206b97f9822c95d08f583c5154745591
Andrew Johnson <python.alj@gmail.com>
2020-12-09 21:37:19 -0800

    Add files via upload

---
+++ Experience_Rewards_Difficulty.patch.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Harmony12;
+using Pathea;
+using UnityEngine;
+using Pathea.MiniGameNs;
+using Pathea.ItemSystem;
+using Pathea.FavorSystemNs;
+using System.Collections.Generic;
+
+namespace Experience_Rewards_Difficulty.patch
+{
+	[HarmonyPatch(typeof(Player), "AddExp", 0)]
+	internal static class PlayerAddExpPatch
+	{
+		private static void Prefix(ref int exp)
+		{
+			if (!Main.Enabled)
+			{
+				return;
+			}
+			exp = Mathf.RoundToInt((float)exp * Main.settings.ExpMultiplier);
+		}
+
+	}
+
+	/*    [HarmonyPatch(typeof(AttrData), "Attack", 0)]
+		internal static class PlayerAttackDamagePatch
+		{
+			private static void Prefix(ref int attack)
+			{
+				if (!Main.Enabled)
+				{
+					return;
+				}
+				attack = Mathf.RoundToInt((float)attack * Main.settings.AttackMultiplier);
+			}
+		}*/
+
+	[HarmonyPatch(typeof(ItemBag), "ChangeMoney", 0)]
+	internal static class PlayerGainMoneyPatch
+	{
+		private static void Prefix(ref int baseValue, bool showTip = true, int extra = 0)
+		{
+			if (!Main.Enabled)
+			{
+				return;
+			}
+
+			baseValue = Mathf.RoundToInt((float)baseValue * Main.settings.PressButton);
+
+
+		}
+
+	}
+
+	[HarmonyPatch(typeof(FavorObject), "GainFavorValue", 0)]
+	internal static class RelationshipPatch
+    {
+		private static void Prefix(ref int gainFavorValue)
+        {
+			if (!Main.Enabled)
+            {
+				return;
+            }
+
+			gainFavorValue = Mathf.RoundToInt((float)gainFavorValue * Main.settings.RelatioshipMuiltiplier);
+		}
+    }
+
+    [HarmonyPatch(typeof(Player), "GainItems", 0)]
+    internal static class PlayerGainItemsPatch
+    {
+        private static void Prefix(ref int money)
+        {
+            if (!Main.Enabled)
+            {
+                return;
+            }
+            money = Mathf.RoundToInt((float)money * Main.settings.MoneyMultiplier);
+
+        }
+
+    }
+
+    [HarmonyPatch(typeof(MiniGameRewardMgr), "GetBalloonReward", 0)]
+	internal static class MiniGameRewardMgrGetBalloonRewardPatch
+	{
+		private static void Postfix(ref List<ItemObject> __result)
+		{
+			if (!Main.Enabled)
+			{
+				return;
+			}
+			foreach (ItemObject itemObject in __result)
+			{
+				int number = Mathf.RoundToInt((float)itemObject.Number * Main.settings.MiniGameRewardMultiplier) - itemObject.Number;
+				itemObject.ChangeNumber(number);
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(MiniGameRewardMgr), "GetDartReward", 0)]
+	internal static class MiniGameRewardMgrGetDartRewardPatch
+	{
+		private static void Postfix(ref List<ItemObject> __result)
+		{
+			if (!Main.Enabled)
+			{
+				return;
+			}
+			foreach (ItemObject itemObject in __result)
+			{
+				int number = Mathf.RoundToInt((float)itemObject.Number * Main.settings.MiniGameRewardMultiplier) - itemObject.Number;
+				itemObject.ChangeNumber(number);
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(MiniGameRewardMgr), "GetSlotMachineReward", new Type[]
+	{
+			typeof(string),
+			typeof(int)
+	})]
+	internal static class MiniGameRewardMgrGetSlotMachineRewardPatch
+	{
+		private static void Postfix(ref List<ItemObject> __result)
+		{
+			if (!Main.Enabled)
+			{
+				return;
+			}
+			foreach (ItemObject itemObject in __result)
+			{
+				int number = Mathf.RoundToInt((float)itemObject.Number * Main.settings.MiniGameRewardMultiplier) - itemObject.Number;
+				itemObject.ChangeNumber(number);
+			}
+		}
+	}
+}
---
+++ Settings.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityModManagerNet;
+
+namespace Experience_Rewards_Difficulty
+{
+	public class Settings : UnityModManager.ModSettings
+	{
+		public float ExpMultiplier
+        {
+            get
+            {
+                return this.expMultiplier;
+            }
+            set
+            {
+                this.expMultiplier = value;
+            }
+        }
+
+        public float MoneyMultiplier
+        {
+            get
+            {
+                return this.moneyMultiplier;
+            }
+            set
+            {
+                this.moneyMultiplier = value;
+            }
+        }
+
+        public float MiniGameRewardMultiplier
+        {
+            get 
+            { 
+                return this.miniGameRewardMultiplier;
+            }
+            set 
+            { 
+                this.miniGameRewardMultiplier = value;
+            }
+        }
+
+/*        public float AttackMultiplier
+        {
+            get { return this.attackMultiplier; }
+            set { this.attackMultiplier = value; }
+        }*/
+
+        public float PressButton
+        {
+            get 
+            {
+                return this.pressButton; 
+            }
+            set 
+            {
+                this.pressButton = value; 
+            }
+        }
+
+        public float RelatioshipMuiltiplier
+        {
+            get
+            {
+                return this.relatioshipMuiltiplier;
+            }
+
+            set
+            {
+                this.relatioshipMuiltiplier = value;
+            }
+        }
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            UnityModManager.ModSettings.Save<Settings>(this, modEntry);
+        }
+
+        private float relatioshipMuiltiplier = 1.5f;
+        private float pressButton = 1.5f;
+        private float moneyMultiplier = 1.5f;
+        private float expMultiplier = 1.5f;
+        private float miniGameRewardMultiplier = 1f;
+        //private float attackMultiplier = 1f;
+
+    }
+}
